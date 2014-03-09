28d9b78e871d31ac98694d8a29258adcb4c3792f
Alvin Kato J.R <alvinkatojr@gmail.com>
2014-03-09 05:41:50 +0300

    Edit profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,19 @@
 class ProfilesController < ApplicationController
   def show
+    if params[:username]
+      @user = User.where(:username => params[username]).first
+      @twets = Twet.by_user_ids(@user.id) if @user
+    else
+      @twets = current_user.all_twets
+    end
+  end
+
+
+  private
+
+  def twet_params
+      params.require(:twet).permit(:content)
   end
 end
+
+
