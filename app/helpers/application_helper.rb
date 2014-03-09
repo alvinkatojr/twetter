354fb1db7bda467314f7a23ad11c02e5a5f4b84c
Alvin Kato J.R <alvinkatojr@gmail.com>
2014-03-09 19:46:43 +0300

    Modify gravatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,13 +4,9 @@ module ApplicationHelper
   # Gravatar helper file
 
   def avatar_url(user)
-    if user.avatar_url.present?
-      user.avatar_url
-    else
-      default_url = "#{root_url}images/guest.png"
-      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=48&d=#{CGI.escape(default_url)}"
-    end
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
   # Responsible for generating the appropriate form to follow or unfollow a user. If the
