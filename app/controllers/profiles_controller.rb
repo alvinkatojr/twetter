544347cb5d1ff9a149ac4737ff508e6985312c10
Alvin Kato J.R <alvinkatojr@gmail.com>
2016-07-16 09:18:44 +0300

    No need for twet params in the profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,13 +2,4 @@ class ProfilesController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
   end
-
-
-  private
-
-  def twet_params
-      params.require(:twet).permit(:content)
-  end
 end
-
-
